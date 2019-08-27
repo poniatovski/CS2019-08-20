02a4d7bdb22e70edaad9da0e97037e6a65932369
poniatovski <butch75@bk.ru>
2019-08-27 12:38:09 +0300

    A1A2A3

--- src/by/it/poniatovski/lesson03/TaskC1.java
+++ src/by/it/poniatovski/lesson03/TaskC1.java
@@ -28,24 +28,16 @@ import java.util.Scanner;
 
 class TaskC1 {
 
-        public static void main(String[] args) {
-            System.out.println(convertCelsiumToFahrenheit(40));
-        }
-    public static double convertCelsiumToFahrenheit(int celsium){
+    public static void main(String[] args) {
+        System.out.println(convertCelsiumToFahrenheit(40));
+    }
+
+    public static double convertCelsiumToFahrenheit(int celsium) {
         double fahrenheit = 9 / 5.0 * celsium + 32;
         return fahrenheit;
 
 
-
-
     }
 
-
-
-
-
-//    public static void main(String[] args) {
-//        System.out.println(convertCelsiumToFahrenheit(40));
-//    }
-
 }
+
