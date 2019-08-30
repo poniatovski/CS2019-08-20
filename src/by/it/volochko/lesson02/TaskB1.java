b93eac2ecb43c84c6a480516ee07ce11b69cd1bf
catherine-1 <volochko.ekaterina@gmail.com>
2019-08-31 00:00:49 +0300

    Lesson2 - tasks B1, B2, B3

--- src/by/it/volochko/lesson02/TaskB1.java
+++ src/by/it/volochko/lesson02/TaskB1.java
@@ -19,5 +19,13 @@ int i=sc.nextInt();
 
  */
 
+import java.util.Scanner;
+
 class TaskB1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int i=sc.nextInt();
+        System.out.println(i*i);
+
+    }
 }
