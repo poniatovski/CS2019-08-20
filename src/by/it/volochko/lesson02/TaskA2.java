eb3b1b366a7c9ed4e93dac30fb80ef765ed20566
catherine-1 <volochko.ekaterina@gmail.com>
2019-08-30 23:45:26 +0300

    Lesson2 - taskA2, taskA3

--- src/by/it/volochko/lesson02/TaskA2.java
+++ src/by/it/volochko/lesson02/TaskA2.java
@@ -9,5 +9,11 @@ package by.it.volochko.lesson02;
 */
 
 class TaskA2 {
+    public static void main(String[] args) {
+        for (int i = 0; i <5; i++) {
+            System.out.println("Я начинаю изучать Java!");
+
+    }
+    }
 
 }
