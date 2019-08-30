f272da24f1ea7fb27f980130fb82530476c8ad5f
Marina Prikhodko <strawberrylove@mail.ru>
2019-08-30 21:37:14 +0300

    lesson 5 taskA and part of taskB complete

--- src/prikhodko/lesson05/TaskB3.java
+++ src/prikhodko/lesson05/TaskB3.java
@@ -19,6 +19,16 @@ Index of last element=1
 
 */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class TaskB3 {
+    public static void main(String[] args) {
+        int[] m = new int[10];
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Arrays.sort(m);
+
+    }
 
 }
