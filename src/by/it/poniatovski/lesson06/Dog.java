39133542151e1b86896e1e80688a7ecc46ac8e62
poniatovski <butch75@bk.ru>
2019-08-31 11:58:24 +0300

    lesson6 a2 b1

--- src/by/it/poniatovski/lesson06/Dog.java
+++ src/by/it/poniatovski/lesson06/Dog.java
@@ -28,4 +28,12 @@ public class Dog {
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "Кличка: "+name+". Возраст: "+age;
+
+
+
+    }
 }
